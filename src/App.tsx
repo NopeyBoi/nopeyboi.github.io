4717f0f8515cd407bbd8f63f7860a64a52237e61
Nopey <hashtagsmrt@outlook.de>
2024-09-05 17:28:48 +1000

    nice

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <>hi, how you doing mate?</>;
+  return (
+    <>
+      <div className="container">
+        <h1 className="text-center m-4 text-danger">You've met with a terrible fate, haven't you?</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
